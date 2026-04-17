be13e8160929d5596e91d8c0bb57732814af5921
MaheshkumarSelvakumar <maheshkumar210723@gmail.com>
2026-04-18 02:18:17 +0530

    Updated Cors policy

--- src/index.js
+++ src/index.js
@@ -11,7 +11,10 @@ const PORT = process.env.PORT || 5000;
 
 // ── MIDDLEWARE ──
 app.use(morgan("dev"));
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL, 
+  credentials: true,
+}));
 app.use(express.json());
 
 // ── ROUTES ──
